88a44dbb7002cf8a1f4daa5ac8c6849bdfa17371
george-kirby <georgeskirby@btinternet.com>
2019-08-14 18:26:37 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 10, "M", "brown", "placid", 1),
+("Rowdy", 8, "M", "brown", "aggressive", 1),
+("Tabitha", 7, "F", "white", "happy", 1),
+("Sergeant Brown", 8, "M", "brown", "placid", 0),
+("Melissa", 5, "F", "black", "placid", 1),
+("Grinch", 10, "M", "white", "aggressive", 1),
+("Wendy", 5, "F", "brown", "placid", 0),
+(NULL, 20, "M", "black", "aggressive", 0);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+-- ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+-- ("Rowdy", 10, "M", "black", "intense", 1),
+-- ("Tabitha", 6, "F", "dark brown", "Nice", 1),
+-- ("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+-- ("Melissa", 13, "F", "dark brown", "goofy", 1),
+-- ("Grinch", 2, "M", "Black", "Grinchy", 1),
+-- ("Wendy", 6, "F", "Blue", "naive", 1),
+-- (null, 20, "M", "black", "aggressive", 0);
