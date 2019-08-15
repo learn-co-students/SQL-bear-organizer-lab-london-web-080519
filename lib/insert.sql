24935980cd71bb85ca44ac9074ab64956881ce74
oliburt <oliburt@gmail.com>
2019-08-15 10:06:37 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, 'male', 'chocolate', 'lazy', 1),
+("Rowdy", 3, 'male', 'dark brown', 'rowdy', 1),
+("Tabitha", 5, 'female', 'cappucino', 'docile', 1),
+("Seargeant Brown", 12, 'male', 'dark caramel', 'dominant', 0),
+("Melissa", 5, 'female', 'caramel', 'happy', 1),
+("Grinch", 8, 'male', 'light brown', 'grumpy', 1),
+("Wendy", 4, 'female', 'mocha', 'protective', 1);
+
+INSERT INTO bears (age, gender, color, temperament, alive)
+VALUES (6, 'male', 'chocolate', 'aggressive', 1);
