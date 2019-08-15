2747f166133c07a538e6d62cc9a5abfe3124c18c
Will Bush <williamdcbush@gmail.com>
2019-08-15 09:53:44 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, gender, color) VALUES ("Mr. Chocolate", "M", "Brown");
+INSERT INTO bears (name, gender, color) VALUES ("Rowdy", "M", "Black");
+INSERT INTO bears (name, gender, color) VALUES ("Tabitha", "F", "Grizzly");
+INSERT INTO bears (name, gender, color) VALUES ("Sergeant Brown", "M", "Brown");
+INSERT INTO bears (name, gender, color) VALUES ("Melissa", "F", "Grizzly");
+INSERT INTO bears (name, gender, color) VALUES ("Grinch", "M", "Polar");
+INSERT INTO bears (name, gender, color) VALUES ("Wendy", "F", "Black");
+INSERT INTO bears (name, gender, color) VALUES (null, "M", "Brown");
