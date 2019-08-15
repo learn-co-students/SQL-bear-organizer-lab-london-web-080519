f12a52ecc29ebeb9327ace25d86acec3cd31f421
tegansims <github email address>
2019-08-15 09:58:11 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1, "Mr Chocolate", 8, "male", "milky brown", "friendly", 1),
+(2, "Mrs Chocolate", 7, "female", "milkier brown", "matronly", 1),
+(3, "Chocolate Offspring the Elder", 4, "female", "lighter brown", "slightly annoyed about having to share her parents' affections", 1),
+(4, "Chocolate Offspring the Younger", 2, "female", "streaky brown", "prone to having toddler-esque meltdowns in the middle of the forest", 1),
+(5, "Mr Unicorn", 9, "male", "silver", "suave, but secretly haunted by Mrs Unicorn's death by hunters", 1),
+(6, "Mrs Unicorn", 9, "female", "silver and brown streaks", NULL, 0),
+(7, "Grandma Chocolate", 16, "female", "dark brown", "old", 1),
+(8, NULL, NULL, "male", "milky brown", "hungry", 1);
